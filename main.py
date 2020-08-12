2565cc1eac59ce6b79ee8befc6ecc9db191084f9
fuston05 <fuston05@yahoo.com>
2020-08-11 20:57:20 -0400

    passes 'sum nums in dict' whiteboarding challenge

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
 # **** whiteBoarding Practice ****
-# Add up and print the sum of the all of the minimum elements of each inner array:
 
-arr= [
-  [8, 4], [90, -1, 3], [9, 62], [-7, -1, -56, -6], [201], [76, 18]
-  ]
-
-# The expected output is given by:
-# 4 + -1 + 9 + -56 + 201 + 18 = 175
-
+# Given an object/dictionary with keys and values that consist of both strings and integers, design an algorithm to calculate and return the sum of all of the numeric values.
+# For example, given the following object/dictionary as input:
+input= {
+  "cat": "bob",
+  "dog": 23,
+  19: 18,
+  90: "fish"
+}
+# Your algorithm should return 41, the sum of the values 23 and 18.
 # You may use whatever programming language you'd like.
 # Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
 
-
-
-# min ti get minumum
-# iterate, for loop
-# sum var to track sum so far
-# print sum
-
-res= 0
-for i in arr:
-  res += min(i)
-
-print(res)
-
-
-
-
-if __name__ == "__main__":
-    pass
+# iterate through input
+result= 0
+for k, v in input.items():
+    print('type; ', type(v))
+    if type(v) == int:
+        result+= v
+print(result)
+    # check type:
+    # if isnum , sum that value
