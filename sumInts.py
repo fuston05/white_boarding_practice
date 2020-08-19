de01a29a8fbf2e8018d2c441743aec61a1c09d3d
fuston05 <fuston05@yahoo.com>
2020-08-19 14:06:48 -0400

    re-organizes files for whiteboarding practice

--- sumInts.py
+++ sumInts.py
@@ -22,30 +22,3 @@
 # # check type:
 # # if isnum , sum that value
 
-# Given the following array of values, print out all the elements in reverse order, with each element on a new line.
-# For example, given the list
-arr= [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-# reverse arr
-# arr.reverse()
-
-# for loop over arr
-for i in range(1, len(arr)+1):
-    print(arr[-i])
-# print each
-
-
-# Your output should be
-# 0
-# 1
-# 2
-# 3
-# 4
-# 5
-# 6
-# 7
-# 8
-# 9
-# 10
-# You may use whatever programming language you'd like.
-# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
