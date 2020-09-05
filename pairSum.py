8ccea9a157892ccc5c749afcadbec84b2d872047
fuston05 <fuston05@yahoo.com>
2020-09-05 19:59:08 -0400

    another iteration, playing around

--- pairSum.py
+++ pairSum.py
@@ -3,38 +3,39 @@
 import random
 import math
 
+
 def pairSum(arr, S):
-    res= []
+    res = []
 
     for i in range(len(arr)):
-      for k in range(len(arr)):
-        if arr[i] + arr[k] == S and [arr[i], arr[k]] not in res and [arr[k], arr[i]] not in res:
-          res.append([arr[i], arr[k]])
-
+        for k in range(len(arr)):
+            if arr[i] + arr[k] == S and [arr[i], arr[k]] not in res and [arr[k], arr[i]] not in res:
+                res.append([arr[i], arr[k]])
 
     return res
-    # print('******')
-    # print('table: ', lookupTable)
-    # print('******')
-      
-
-
 
 if __name__ == "__main__":
-  # get a random 'S'
-    S= math.floor(random.random()*10)
-    
-    ranLength= math.floor(random.random()* 10)*(math.floor(random.random()*10))
 
-    # randArr= []
-    # for i in range(ranLength):
-    #     if i not in randArr:
-    #       randArr.append(math.floor(random.random()*10))
+    # get a random 'S'
+    # S = math.floor(random.random()*10) > 10 or 10
 
-    randArr= [(math.floor(random.random()*10)) for x in range(ranLength)]
-
+    S= math.floor( random.randrange(10, random.randint(10, 50)) )
     print('S: ', S)
     print('')
-    print('arr: ', randArr)
-    print('')
-    print('func call: ', pairSum(randArr, S))
+
+    ranLength = 0
+    while ranLength < 9:
+        ranLength = (math.floor(random.random()*10)) * \
+            (math.floor(random.random()*10))
+
+        randArr = []
+        for i in range(5, ranLength):
+            temp = math.floor((random.random()*10) * (random.random()*10))
+            if int(temp) not in randArr:
+                randArr.append(temp)
+
+        # randArr= [math.floor(random.random()*10) for x in range(ranLength)]
+
+        print('randArr: ', randArr)
+        print('')
+        print('func call: ', pairSum(randArr, S))
