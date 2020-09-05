3dbacab2866836009487cf1e6b9a53ec931c1b30
fuston05 <fuston05@yahoo.com>
2020-09-05 15:39:15 -0400

    test

--- pairSum.py
+++ pairSum.py
@@ -1,27 +1,40 @@
 # Find all the pairs of two integers in an unsorted array that sum up to a given S. For example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7, your program should return [[11, -4], [2, 5]] because 11 + -4 = 7 and 2 + 5 = 7.
 # challenge from Lambda Endorsement Interview:
+import random
+import math
 
 def pairSum(arr, S):
-    result= []
+    res= []
 
-    # if empty return None
-    if len(arr) == 0:
-        return None
-
-    # iterate over arr
     for i in range(len(arr)):
-    #   inner loop
-        for k in range(1, len(arr)-1):
-        # this allows to compare 
-            if arr[i] + arr[k] == S and [arr[i], arr[k]] not in result or [arr[ k], arr[i]] not in result:
+      for k in range(len(arr)):
+        if arr[i] + arr[k] == S and [arr[i], arr[k]] not in res and [arr[k], arr[i]] not in res:
+          res.append([arr[i], arr[k]])
 
-            # if combo matches S
-                result.append([arr[i], arr[k]])
 
-    return result
+    return res
+    # print('******')
+    # print('table: ', lookupTable)
+    # print('******')
+      
+
+
 
 if __name__ == "__main__":
-    S= 7
-    arr= [3, 5, 2, -4, 8, 11] 
+  # get a random 'S'
+    S= math.floor(random.random()*10)
+    
+    ranLength= math.floor(random.random()* 10)*(math.floor(random.random()*10))
 
-    print(pairSum(arr, S))
+    # randArr= []
+    # for i in range(ranLength):
+    #     if i not in randArr:
+    #       randArr.append(math.floor(random.random()*10))
+
+    randArr= [(math.floor(random.random()*10)) for x in range(ranLength)]
+
+    print('S: ', S)
+    print('')
+    print('arr: ', randArr)
+    print('')
+    print('func call: ', pairSum(randArr, S))
