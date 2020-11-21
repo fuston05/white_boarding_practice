8db5f63985e130677324e7e30ea42cc07095de2c
scott fuston <fuston05@yahoo.com>
2020-11-21 08:38:56 -0500

    adds a 2nd-pass refactor solution to isIsomorphic leetcode challenge

--- complete/isIsomorphic.js
+++ complete/isIsomorphic.js
@@ -6,6 +6,55 @@
 
 // All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
 
+// 1st pass solution:
+// var isIsomorphic = function (s, t) {
+//   string1 = Array.from(s);
+//   string2 = Array.from(t);
+//   if (string1.length !== string2.length) {
+//     return false;
+//   }
+
+//   let key = [];
+//   for (let i = 0; i < 28; i++) {
+//     key.push(Math.random() * i);
+//   }
+
+//   let map1 = {};
+//   let map2 = {};
+//   let pattern1 = [];
+//   let pattern2 = [];
+//   let keyOneCount = 0;
+//   let keyTwoCount = 0;
+
+//   //     map for string1
+//   for (let i = 0; i < string1.length; i++) {
+//     //      pattern1
+//     if (!map1.hasOwnProperty(string1[i])) {
+//       map1[string1[i]] = key[keyOneCount];
+//       keyOneCount++;
+//     }
+//     pattern1.push(map1[string1[i]]);
+//   }
+
+//   //     map for string2
+//   for (let j = 0; j < string2.length; j++) {
+//     //      pattern2
+//     if (!map2.hasOwnProperty(string2[j])) {
+//       map2[string2[j]] = key[keyTwoCount];
+//       keyTwoCount++;
+//     }
+//     pattern2.push(map2[string2[j]]);
+//   }
+
+//   for (let i = 0; i < pattern1.length; i++) {
+//     if (pattern1[i] !== pattern2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// 2nd pass solution:
 var isIsomorphic = function (s, t) {
   string1 = Array.from(s);
   string2 = Array.from(t);
@@ -13,6 +62,7 @@ var isIsomorphic = function (s, t) {
     return false;
   }
 
+  //  generate keys for mapping
   let key = [];
   for (let i = 0; i < 28; i++) {
     key.push(Math.random() * i);
@@ -28,28 +78,17 @@ var isIsomorphic = function (s, t) {
   //     map for string1
   for (let i = 0; i < string1.length; i++) {
     //      pattern1
-    if (!map1.hasOwnProperty(string1[i])) {
+    if (!map1.hasOwnProperty(string1[i]) && !map2.hasOwnProperty(string2[i])) {
       map1[string1[i]] = key[keyOneCount];
+      map2[string2[i]] = key[keyTwoCount];
       keyOneCount++;
-    }
-    pattern1.push(map1[string1[i]]);
-  }
-
-  //     map for string2
-  for (let j = 0; j < string2.length; j++) {
-    //      pattern2
-    if (!map2.hasOwnProperty(string2[j])) {
-      map2[string2[j]] = key[keyTwoCount];
       keyTwoCount++;
     }
-    pattern2.push(map2[string2[j]]);
+    pattern1.push(map1[string1[i]]);
+    pattern2.push(map2[string2[i]]);
   }
+  if (JSON.stringify(pattern1) !== JSON.stringify(pattern2)) return false;
 
-  for (let i = 0; i < pattern1.length; i++) {
-    if (pattern1[i] !== pattern2[i]) {
-      return false;
-    }
-  }
   return true;
 };
 
@@ -64,8 +103,8 @@ var isIsomorphic = function (s, t) {
 // Output: false
 
 // Example 3:
-// s = "paper"
-// t = "title"
+s = "paper"
+t = "title"
 // Output: true
 // Note:
 // You may assume both s and t have the same length.
