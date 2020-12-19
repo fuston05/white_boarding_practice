5f21c7371670cf88ce895ea08db59f5cab8c9b2a
scott fuston <fuston05@yahoo.com>
2020-12-19 11:43:17 -0500

    isPallindrome using for of loop

--- complete/isPalindrome.js
+++ complete/isPalindrome.js
@@ -1,12 +1,16 @@
-
 const isPalindrome = (str) => {
-  return str === str.split('').reverse().join('');
-}
+  // first pass
+  // return str === str.split("").reverse().join("");
 
+  let revStr = "";
+  for (let char of str) {
+    revStr = char + revStr;
+  }
+  return revStr === str;
+};
 
 // test cases
-str = 'racecar';
-// str = 'hello';
-
+str = "racecar";
+// str = "hello";
 
 console.log(isPalindrome(str));
