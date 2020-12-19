709316a47087a49843f07bbfaed63d8a3b950f15
scott fuston <fuston05@yahoo.com>
2020-12-19 11:25:46 -0500

    reverseString using ES6 reduce

--- complete/reverseString.js
+++ complete/reverseString.js
@@ -7,13 +7,19 @@ const reverseString = (str) => {
   // for (let i = 0; i < str.length; i++){
   //   revStr = str[i] + revStr;
   // }
+  // return revStr;
 
   // decrementing for loop
-  let revStr = '';
-  for (let i = str.length-1; i >= 0; i--){
-    revStr = revStr + str[i];
-  }
-  return revStr;
+  // let revStr = '';
+  // for (let i = str.length-1; i >= 0; i--){
+  //   revStr = revStr + str[i];
+  // }
+  // return revStr;
+
+  //  reduce
+  return str.split("").reduce((revStr, char) => {
+    return char + revStr;
+  });
 };
 
 // test cases
