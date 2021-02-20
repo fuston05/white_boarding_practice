033c5ea3b2d5b177b8d8bc5e9b61c7a9a06e01fc
scott fuston <fuston05@yahoo.com>
2021-02-20 18:52:43 -0500

    had to look up some help, but I got gcdOfStrings solved!

--- incomplete/gcdOfStrings.js
+++ incomplete/gcdOfStrings.js
@@ -9,48 +9,44 @@
  */
 
 var gcdOfStrings = function (str1, str2) {
-  let result = "";
-  // all possible 'unique' letters in str2
-  let letters = [str2[0]];
-  let shortestSubStr = "";
-  let curLongestSubStr = str2;
+  let aLen = str1.length;
+  let bLen = str2.length;
 
-  // buile letters arr from str2
-  for (let i = 1; i < str2.length; i++) {
-    if (!letters.includes(str2[i])) {
-      letters.push(str2[i]);
-    }
+  // find the GCD of the 2 lengths
+  let gcd = getGCD(aLen, bLen);
+
+  let aSub = str1.slice(0, gcd);
+  let bSub = str2.slice(0, gcd);
+
+  if (
+    str1 === bSub.repeat(str1.length / gcd) &&
+    str2 === aSub.repeat(str2.length / gcd)
+  ) {
+    return aSub;
   }
-  console.log("letters: ", letters);
-
-  // find shortest possible substr in str2
-  let temp = "";
-  let letterCount = 0;
-
-  for (let i = 0; letterCount < letters.length; i++) {
-    console.log("temp: ", temp);
-    if (!temp.includes(str2[i])) {
-      letterCount++;
-    }
-    temp += str2[i];
-  }
-  shortestSubStr = temp;
-  console.log("temp: ", temp);
-
-
-
-
-
+  return '';
 };
 
-
-
-
+// helper function to get the GCD of strring lengths
+const getGCD = (a, b) => {
+  for (let i = a; i > 0; i--) {
+    if (a % i === 0 && b % i === 0) {
+      return i;
+    }
+  }
+};
 
 // TEST CASES
-// str1 = "ABCABC", str2 = "ABC" //"ABC"
-(str1 = "ABABAB"), (str2 = "ABAB"); // "AB"
-// str1 = "LEET", str2 = "CODE" // ""
-// str1 = "ABCDEF", str2 = "ABC" // ""
+// let str1 = "ABCABC"; 
+// let str2 = "ABC"; //"ABC"
+
+// let str1 = "ABABAB";
+// let str2 = "ABAB"; // "AB"
+
+// let str1 = "LEET"; 
+// let str2 = "CODE" // ""
+
+let str1 = "ABCDEF"; 
+let str2 = "ABC" // ""
 
 console.log(gcdOfStrings(str1, str2));
