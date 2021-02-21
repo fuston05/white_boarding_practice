59d9160526735a15732bda0daa14306af53ea954
scott fuston <fuston05@yahoo.com>
2021-02-21 15:25:59 -0500

    adds the desctiption to 'gdcOfStrings' challenge

--- complete/gcdOfStrings.js
+++ complete/gcdOfStrings.js
@@ -1,6 +1,9 @@
 // leet code challenge
 
 // GREATEST COMMON DIVISOR OF STRINGS
+// For two strings s and t, we say "t divides s" if and only if s = t + ... + t  (t concatenated with itself 1 or more times)
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
 
 /**
  * @param {string} str1
