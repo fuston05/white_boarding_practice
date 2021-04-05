4d6cc20cd980fa413068a8a5694bb0686d728294
scott fuston <fuston05@yahoo.com>
2021-04-05 18:33:41 -0400

    sets up leetcode challenge "longestCommonPrefix"

---
+++ incomplete/longestCommonPrefix.js
@@ -0,0 +1,31 @@
+// leetcode challenge (easy)
+// longestCommonPrefix
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Constraints:
+
+// 0 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lower-case English letters.
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+
+};
+
+// Example 1:
+let strs = ["flower", "flow", "flight"];
+// Output: "fl"
+
+// Example 2:
+// let strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+console.log(longestCommonPrefix(strs))
