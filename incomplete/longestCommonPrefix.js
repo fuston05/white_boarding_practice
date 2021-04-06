37d30295409c804e9b3ae36a6c08524608a8aa80
scott fuston <fuston05@yahoo.com>
2021-04-05 20:05:47 -0400

    failed attemp at longestCommonPrefix

--- incomplete/longestCommonPrefix.js
+++ incomplete/longestCommonPrefix.js
@@ -16,7 +16,36 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
+  // strs could be empty
+  // any item in strs could be empty.
+  // gonna need a nested loop?? against length of the shortest word in strs.
 
+  if (strs.length === 0) {
+    return "";
+  }
+
+  // find  the shortest word and start that as longest so far??
+  let shortest = [];
+  for (let i = 0; i < strs.length - 1; i++) {
+    if (strs[i].length < strs[i + 1].length) {
+      shortest = strs[i].split("");
+    }
+  }
+
+  let count = 0;
+  while (count < (shortest.length * strs.length)) {
+    let isNotCommon = true;
+    strs.forEach((str) => {
+      if (!str.includes(shortest.join(""))) {
+        isNotCommon = true;
+      } else {
+        isNotCommon = false;
+      }
+    });
+    isNotCommon ? shortest.pop() : null;
+    count++;
+  }
+  return shortest.join("");
 };
 
 // Example 1:
@@ -24,8 +53,8 @@ let strs = ["flower", "flow", "flight"];
 // Output: "fl"
 
 // Example 2:
-// let strs = ["dog","racecar","car"]
+// let strs = ["dog", "racecar", "car"];
 // Output: ""
 // Explanation: There is no common prefix among the input strings.
 
-console.log(longestCommonPrefix(strs))
+console.log("result: ", longestCommonPrefix(strs));
