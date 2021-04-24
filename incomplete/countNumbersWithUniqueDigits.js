6c924fb7deb185a98554a7a275059cbc3824055f
scott fuston <fuston05@yahoo.com>
2021-04-24 16:02:02 -0400

    working on leetcode challenge countNumbersWithUniqueDigits

---
+++ incomplete/countNumbersWithUniqueDigits.js
@@ -0,0 +1,43 @@
+// leetcode challenge (medium)
+// count numbers with unique digits
+
+// Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10n.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countNumbersWithUniqueDigits = function (n) {
+  let count = 0;
+
+  console.log(`\n10^${n}: `, 10 ** n, "\n");
+
+  for (let i = count; i < 10 ** n; i++) {
+    console.log("\nloopCount: ", i, "\n");
+    // check if digits are unique for given number
+
+    console.log("count %: ", 0 % 11);
+    if (i > 0 && i % 11 === 0) {
+      count++;
+    }
+  }
+
+  return 10 ** n - count;
+};
+
+// Example 1:
+// let n = 2;
+// Output: 91
+// Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, excluding 11,22,33,44,55,66,77,88,99
+
+// Example 2:
+// let n = 0;
+// Output: 1
+
+// Example 3:
+let n = 3;
+
+// Constraints:
+// 0 <= n <= 8
+
+console.log(countNumbersWithUniqueDigits(n));
