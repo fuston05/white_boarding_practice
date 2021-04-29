599735d6788bc520ef9e8c16279f4f316ca06f5d
scott fuston <fuston05@yahoo.com>
2021-04-29 15:44:13 -0400

    progresses leetcode challenge checkStraightLine, still failing example 3 input

--- incomplete/checkStraightLine.js
+++ incomplete/checkStraightLine.js
@@ -9,27 +9,39 @@
  */
 var checkStraightLine = function (coordinates) {
   //  determine increment used for both X and Y between the 1st 2 coords
+  let X_incPattern= coordinates[1][0] - coordinates[0][0];
+  let Y_incPattern= coordinates[1][1] - coordinates[0][1];
   
-
-  let firstX = coordinates[0][0];
-  let firstY = coordinates[0][1];
-
-  console.log(`firstX: ${firstX}, \nfirstY: ${firstY}`)
-
+  console.log(`X_incPattern: ${X_incPattern}, \n Y_incPattern: ${Y_incPattern}`)
+  
   //  check that same increment stays consistant throughout.
   //      if yes: it's straight
   //      if no: it's not straight
+  for (let i = 0; i < coordinates.length-1; i++){
+    // if 2nd x - 1st X not = increment pattern or 2nd Y - 1st Y not == pattern
+      // return false
+    // console.log(`2nd X: ${coordinates[i + 1][0]}, 1st X: ${coordinates[i][0]}, Diff: ${coordinates[i + 1][0] - coordinates[i][0]}`)
+    // console.log('');
+    // console.log(`2nd Y: ${coordinates[i + 1][1]}, 1st Y: ${coordinates[i][1]}, Diff: ${coordinates[i + 1][1] - coordinates[i][1]}`)
+    // console.log('*****');
+
+    if (coordinates[i+1][0] - coordinates[i][0] !== X_incPattern
+      || coordinates[i+1][1] - coordinates[i][1] !== Y_incPattern) {
+      return false
+      }
+  }
+  return true;
 };
 
 // Example 1:
-let coordinates = [
-  [1, 2],
-  [2, 3],
-  [3, 4],
-  [4, 5],
-  [5, 6],
-  [6, 7],
-];
+// let coordinates = [
+//   [1, 2],
+//   [2, 3],
+//   [3, 4],
+//   [4, 5],
+//   [5, 6],
+//   [6, 7],
+// ];
 // Output: true
 
 // Example 2:
@@ -42,6 +54,14 @@ let coordinates = [
 //   [7, 7],
 // ];
 // Output: false
+
+// Example 3:
+let coordinates = [
+  [0, 0],
+  [0, 1],
+  [0, -1]
+];
+// Output: true
 
 // Constraints:
 // 2 <= coordinates.length <= 1000
