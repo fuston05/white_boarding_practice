3de8f2e014cc5fffb1dd74c915fe240ad0bed60f
scott fuston <fuston05@yahoo.com>
2021-05-12 17:34:07 -0400

    saves progress, I will come back to this after more study on permutations

--- incomplete/diStringMatch.js
+++ incomplete/diStringMatch.js
@@ -1,5 +1,6 @@
 // leetcode challenge (easy)
 // 942. DI String Match
+// https://leetcode.com/problems/di-string-match/
 
 // A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
 
@@ -12,7 +13,36 @@
  * @return {number[]}
  */
 var diStringMatch = function (s) {
-  
+  // perm= [0, 1, 2, 3, 4] for s= "I  D  I  D" : length= 4
+  //           I and D equate to: <, >, <, >
+  //        I  I  I  I  I .. this makes no sense? .. so, this is an invalid perm.. NEXT
+  // how are they ordering the arr of ints????
+  // I got it! it's compared to the string s to be valid or invalid perm
+
+  // so I= <
+  // D= >
+  // numbers just need to fit the equation of <, >, <, > respectively
+
+  // 1: form a perm
+  for (let i = 0; i <= s.length; i++){
+    console.log('I: ', i)
+    let perm = [];
+    perm.push(i)
+
+    for (let j = 0; j <= s.length; j++){
+      if (i !== j) {
+        perm.push(j)
+      }
+    }
+    console.log('perm: ', perm)
+  }
+  // 2: check it against the formula of <'s and >'s
+
+
+  // for length+1 of perm:
+    // if perm[0](0) < perm[0+1](1) : s[0]= 'I'
+    // if perm[0](0) > perm[0+1](1) : s[0]= 'D'
+
 };
 
 // Example 1:
