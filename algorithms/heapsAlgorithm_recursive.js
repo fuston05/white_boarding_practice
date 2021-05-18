4469ae2f8021df06a37fc2410e56e10c36e19b4d
scott fuston <fuston05@yahoo.com>
2021-05-17 20:51:30 -0400

    adds a follow-along of an iteratvie solution for Heap's algorithm

--- algorithms/heapsAlgorithm_recursive.js
+++ algorithms/heapsAlgorithm_recursive.js
@@ -1,35 +1,78 @@
 // heaps algorithm (permutations)
 // https://en.wikipedia.org/wiki/Heap%27s_algorithm
+
 // runtime: O(n!)
 
+// RECURSIVE VERSION
+// const getPermutations = (arr) => {
+//   const output = [];
+
+//   const swapInPlace = (arrToSwap, indA, indB) => {
+//     [arrToSwap[indA], arrToSwap[indB]] = [arrToSwap[indB], arrToSwap[indA]];
+//   };
+
+//   const generate = (n, heapArr) => {
+//     if (n === 1) {
+//       output.push(heapArr.slice());
+//       return;
+//     }
+
+//     generate(n - 1, heapArr);
+
+//     for (let i = 0; i < n - 1; i++) {
+//       if (n % 2 === 0) {
+//         swapInPlace(heapArr, i, n - 1);
+//       } else {
+//         swapInPlace(heapArr, 0, n - 1);
+//       }
+//       generate(n - 1, heapArr);
+//     }
+//   };
+
+//   generate(arr.length, arr.slice());
+
+//   return output;
+// };
+
+// ITERATIVE SOLUTION
 const getPermutations = (arr) => {
-  const output = [];
+  let result = [];
+  let len = arr.length;
+  const arrCopy = arr.slice();
+  let C = new Array(len).fill(0);
 
-  const swapInPlace = (arrToSwap, indA, indB) => {
-    [arrToSwap[indA], arrToSwap[indB]] = [arrToSwap[indB], arrToSwap[indA]];
-  };
+  result.push(arrCopy);
 
-  const generate = (n, heapArr) => {
-    if (n === 1) {
-      output.push(heapArr.slice());
-      return;
-    }
+  let i = 0;
 
-    generate(n - 1, heapArr);
+  while (i < len) {
 
-    for (let i = 0; i < n - 1; i++) {
-      if (n % 2 === 0) {
-        swapInPlace(heapArr, i, n - 1);
+    if (C[i] < i) {
+      // if even
+      if (i % 2 === 0) {
+        swap(arrCopy, 0, i);
       } else {
-        swapInPlace(heapArr, 0, n - 1);
+        // if odd
+        swap(arrCopy, C[i], i);
       }
-      generate(n - 1, heapArr);
+
+      result.push(arrCopy.slice());
+      C[i] += 1;
+      i = 0;
+
+    } else { // if C[i] >= i
+      C[i] = 0;
+      i += 1;
     }
-  };
+  }
 
-  generate(arr.length, arr.slice());
+  function swap(arrToSwap, indA, indB) {
+    [arrToSwap[indB], arrToSwap[indA]] = [arrToSwap[indA], arrToSwap[indB]];
+  }
 
-  return output;
+  return result;
 };
 
+console.log("");
 console.log(getPermutations([1, 2, 3, 4]));
+console.log("");
